66d737a6cb64405fbb24e8ecbe8f5ecb2070f8a0
Alex Palcuie <alex.palcuie@hootsuite.com>
2016-01-04 14:58:40 +0200

    Write example

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-$(function() {
-  console.log("hello world")
-});
+function parseResults(datasets) {
+    $list = $('#list')
+    for (var i in datasets) {
+        $list.append('<li>' + datasets[i] + '</li>')
+    }
+}
+
+function onButtonClick(event) {
+    $.ajax({
+        url: "http://data.gov.ro/api/3/action/package_list",
+        dataType: "jsonp",
+        success: function(response) {
+            parseResults(response.result)
+        },
+    })
+}
+
